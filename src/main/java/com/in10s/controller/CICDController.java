b52d604de33ca4442229276b09d1fe563941eb57
Abhishek Amar <abhishek.amar>
2022-04-18 18:57:53 +0530

    Added 2 apis findall & find by id

--- src/main/java/com/in10s/controller/CICDController.java
+++ src/main/java/com/in10s/controller/CICDController.java
@@ -3,16 +3,23 @@
  */
 package com.in10s.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.in10s.pojo.Employee;
+import com.in10s.utils.Util;
 
 /**
  * @author Abhishek Amar
@@ -29,11 +36,28 @@ public class CICDController {
 		map.put("subject", "creating the cicd pipeline");
 		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
-	/* for dummy save*/
+
+	/* for dummy save */
 	@PostMapping
-	ResponseEntity<Map<String, String>> saveDetails(@RequestBody Map<String,String> map) { 
+	ResponseEntity<Map<String, String>> saveDetails(@RequestBody Map<String, String> map) {
 		map.put("message", "Saved successfully");
 		map.put("status", "OK");
 		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
+
+	@GetMapping("/find-by-id/{id}")
+	ResponseEntity<List<Employee>> findDetailsById(@PathVariable("id") String id) { 
+		List<Employee> employeeList = Util.getEmployeeList();
+		List<Employee> empLists = employeeList.stream().filter(emp -> emp.getEmployeeId().equals(id))
+				.collect(Collectors.toList());
+		return new ResponseEntity<>(empLists, HttpStatus.OK);
+	}
+
+	@PostMapping("/find-all")
+	ResponseEntity<List<Employee>> findAll() {  
+		return new ResponseEntity<>(Util.getEmployeeList(), HttpStatus.OK);
+	}
+	
+	
+
 }
